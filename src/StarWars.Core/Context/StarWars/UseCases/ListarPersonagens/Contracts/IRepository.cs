d368b127d2568eff5ac43db6b61f5b93fe38c2da
Rafael Travagin <rafael.travagin.ti@gmail.com>
2024-04-25 21:06:24 -0300

    feat: Adicionado pastas no UseCase e classe Response Base, e IRepository

---
+++ src/StarWars.Core/Context/StarWars/UseCases/ListarPersonagens/Contracts/IRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWars.Core.Context.StarWars.UseCases.ListarPersonagens.Contracts;
+
+public interface IRepository
+{
+    
+}
---
+++ src/StarWars.Core/Context/StarWars/UseCases/ListarPlanetas/Contracts/IRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWars.Core.Context.StarWars.UseCases.ListarPlanetas.Contracts;
+
+public interface IRepository
+{
+    
+}
