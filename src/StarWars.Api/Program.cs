6c8848988fd02759e3833df815e19b2c3badc513
Rafael Travagin <rafael.travagin.ti@gmail.com>
2024-05-01 11:13:51 -0300

    feeat: adicionado swagger

--- src/StarWars.Api/Program.cs
+++ src/StarWars.Api/Program.cs
@@ -8,11 +8,21 @@ builder.AddDatabase();
 builder.AddCorsConfiguration();
 builder.AddStarWarsContext();
 builder.AddMediator();
+builder.AddSwaggerConfiguration();
 
 var app = builder.Build();
 app.UseCors(Configuration.Cors.CorsPolicyName);
 app.UseHttpsRedirection();
 app.MapStarWarsEndpoints();
 app.MapGet("/", () => "Hello World!");
+
+app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "StarWarsInfoApi";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
 
 app.Run();
